3a36b298a62167c9091b625e91394adc8d97655a
saif <saif.iqbal_2007@hotmail.com>
2016-03-27 19:52:06 +0500

    demo patient portal Views added

---
+++ UI/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('main', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/',{
+        templateUrl: 'PatientAccount.html',
+        controller: 'LoginCtrl'
+    }).when('/login', {
+        templateUrl: 'Login.html',
+        controller: 'LoginCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    });
+}]);
+
+
+app.controller('LoginCtrl', function ($scope) {
+
+    $scope.message = 'This is Add new order screen';
+
+});
