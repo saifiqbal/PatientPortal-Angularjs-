876396a7c03f57fa3467fc65b47eba40a23ec7c3
saif <saif.iqbal_2007@hotmail.com>
2016-03-27 19:58:32 +0500

    This commit contains packages webservice. Repository is remaining.

---
+++ DemoPatienPortal/Controllers/IntakeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Repository;
+using Repository.GenericRepository;
+using Repository.UnitOfWork;
+using DemoPatienPortal.Models;
+using System.Web.Script.Serialization;
+
+namespace DemoPatienPortal.Controllers
+{
+    public class IntakeController : ApiController
+    {
+        //private UnitOfWork _UnitOfWork = null; 
+        public IntakeController()
+        {
+            _UnitOfWork = new UnitOfWork();
+        }  
+        // GET: api/Intake
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Intake/5
+        public List<IntakeModel> Get(int id)
+        {
+            List<IntakeModel> intakes = new List<IntakeModel>();
+            //var data= _UnitOfWork.Repository<op_Intake>().GetAll().ToList();
+            //foreach (var intakedata in data)
+            //{
+            //    IntakeModel intakemodel = new IntakeModel();
+            //    intakemodel.IntakeID = intakedata.IntakeID;
+            //    intakemodel.CreatedBy = intakedata.CreatedBy;
+            //    intakemodel.IsDeleted = intakedata.IsDeleted;
+            //    intakemodel.ResponseID = intakedata.ResponseID;
+            //    intakemodel.UniqueValue = intakedata.UniqueValue;
+            //    intakemodel.UserID = intakedata.UserID;
+            //    intakes.Add(intakemodel);
+            //}
+
+
+
+            //var jsonSerialiser = new JavaScriptSerializer();
+            //var json = jsonSerialiser.Serialize(intakes);
+
+            return intakes;
+        }
+
+        // POST: api/Intake
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Intake/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Intake/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoPatienPortal/Models/IntakeModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoPatienPortal.Models
+{
+    public class IntakeModel
+    {
+        public IntakeModel()
+        {
+            //this.op_NIHStrokeScale = new HashSet<op_NIHStrokeScale>();
+            //this.op_PatientRefund = new HashSet<op_PatientRefund>();
+            //this.op_PatientSatisfactionSurvey = new HashSet<op_PatientSatisfactionSurvey>();
+            //this.op_PhysicianAddendum = new HashSet<op_PhysicianAddendum>();
+            //this.op_PhysicianConsult = new HashSet<op_PhysicianConsult>();
+            //this.PhysicianConsults = new HashSet<PhysicianConsult>();
+        }
+
+        public int IntakeID { get; set; }
+        public System.Guid UniqueValue { get; set; }
+        public bool IsDeleted { get; set; }
+        public Nullable<int> UserID { get; set; }
+        public Nullable<int> ResponseID { get; set; }
+        public Nullable<int> CreatedBy { get; set; }
+       
+        //public virtual rz_User rz_User { get; set; }
+        //public virtual rz_Response rz_Response { get; set; }
+        //public virtual rz_User rz_User1 { get; set; }
+        //public virtual ICollection<op_NIHStrokeScale> op_NIHStrokeScale { get; set; }
+        //public virtual ICollection<op_PatientRefund> op_PatientRefund { get; set; }
+        //public virtual ICollection<op_PatientSatisfactionSurvey> op_PatientSatisfactionSurvey { get; set; }
+        //public virtual ICollection<op_PhysicianAddendum> op_PhysicianAddendum { get; set; }
+        //public virtual ICollection<op_PhysicianConsult> op_PhysicianConsult { get; set; }
+        //public virtual ICollection<PhysicianConsult> PhysicianConsults { get; set; }
+    }
+}
